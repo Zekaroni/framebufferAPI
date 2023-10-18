2f3ba6a8fca5cce26de57b54816f16f6f00e21b2
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-18 13:30:09 -0400

    optimized local buffer

--- fb.py
+++ fb.py
@@ -3,22 +3,28 @@ HEIGHT = 800
 BYTES_PER_PIXEL = 4
 SYS_VIDEO_BUFFER = open("/dev/fb0","r+b")
 LOCAL_BUFFER = SYS_VIDEO_BUFFER.read()
+LOCAL_QUEUE = [] # Stores arrays that contain [position, Byte]
 
-def writePixel(x: int, y: int, Bytes: bytes, local_buff = True):
-    global LOCAL_BUFFER
+def writePixel(x: int, y: int, Bytes: bytes):
     if 0 <= x < WIDTH and 0 <= y < HEIGHT:
         position = (y * WIDTH + x) * BYTES_PER_PIXEL
-        if local_buff:
-            buffer_list = bytearray(LOCAL_BUFFER)
-            buffer_list[position:position+BYTES_PER_PIXEL] = Bytes
-            LOCAL_BUFFER = bytes(buffer_list)
-        else:
-            SYS_VIDEO_BUFFER.seek(position)
-            SYS_VIDEO_BUFFER.write(Bytes)
+        SYS_VIDEO_BUFFER.seek(position)
+        SYS_VIDEO_BUFFER.write(Bytes)
 
 def writeBuffer(Bytes: bytes, position: int = 0) -> None:
     SYS_VIDEO_BUFFER.seek(position)
     SYS_VIDEO_BUFFER.write(Bytes)
+
+def queueLocalChange(x: int, y: int, Bytes: bytes):
+    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
+        position = (y * WIDTH + x) * BYTES_PER_PIXEL
+        LOCAL_QUEUE.append([position,Bytes])
+        
+def updateLocalBuffer() -> None:
+    buffer_list = bytearray(LOCAL_BUFFER)
+    for position, Bytes in LOCAL_QUEUE:
+        buffer_list[position:position+BYTES_PER_PIXEL] = Bytes
+    LOCAL_BUFFER = bytes(buffer_list)
 
 def syncBuffers() -> None:
     SYS_VIDEO_BUFFER.seek(0)
