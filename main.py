738402febfed5fc7d8e231d88bfa0be4efc48f12
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-18 13:34:16 -0400

    more performance

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ def drawSquare(size: int, start_x: int, start_y: int, Byte: bytes):
     for j in range(start_y, start_y + size):
         for i in range(start_x, start_x + size):
             fb.queueLocalChange(i, j, Byte)
-    fb.updateLocalBuffer()
-    fb.syncBuffers()
 
 drawSquare(100,0,0,b'\x00\xff\xff\x00')
 drawSquare(100,100,0,b'\x44\xfe\xd2\x00')
 drawSquare(400,200,0,b'\x00\x00\xFF\x00')
 drawSquare(200,0,100,b'\xFF\x00\x00\x00')
+fb.updateLocalBuffer()
+fb.syncBuffers()
