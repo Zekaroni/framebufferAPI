a4eebaac8384e2256db81d98098d3d074c5380bb
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-19 01:41:50 -0400

    vm

--- fb.py
+++ fb.py
@@ -5,6 +5,13 @@ SYS_VIDEO_BUFFER = open("/dev/fb0","r+b")
 LOCAL_BUFFER = SYS_VIDEO_BUFFER.read()
 LOCAL_QUEUE = [] # TODO: Maybe use a dict so the position is only written to once, using the latest value it has recieved
 
+COLOURS = {
+    "RED": b'\x00\x00\xFF\x00',
+    "GREEN": b'\x00\xFF\x00\x00',
+    "BLUE": b'\xFF\x00\x00\x00',
+    "PURPLE": b'\xFF\x00\xFF\x00',
+}
+
 def writePixel(x: int, y: int, Bytes: bytes):
     if 0 <= x < WIDTH and 0 <= y < HEIGHT:
         position = (y * WIDTH + x) * BYTES_PER_PIXEL
@@ -43,16 +50,23 @@ def initTerminal() -> None:
     # clearFrameBuffer()
     print("\n"*(HEIGHT//14))
 
-def drawSquare(size: int, start_x: int, start_y: int, Byte: bytes):
+def drawSquare(size: int, start_x: int, start_y: int, colour: bytes) -> None:
     for j in range(start_y, start_y + size):
         for i in range(start_x, start_x + size):
-            queueLocalChange(i, j, Byte)
+            queueLocalChange(i, j, colour)
+
+
+def drawRectangle(size_x: int, size_y: int, start_x: int, start_y: int, colour: bytes) -> None:
+    for j in range(start_y, start_y + size_x):
+        for i in range(start_x, start_x + size_y):
+            queueLocalChange(i, j, colour)
 
 
 initTerminal()
-drawSquare(100,0,0,b'\x00\xff\xff\x00')
-drawSquare(100,100,0,b'\x44\xfe\xd2\x00')
-drawSquare(400,200,0,b'\x00\x00\xFF\x00')
-drawSquare(200,0,100,b'\xFF\x00\x00\x00')
+drawSquare(100,0,0,COLOURS["RED"])
+drawSquare(100,100,0,COLOURS["GREEN"])
+drawSquare(400,200,0,COLOURS["BLUE"])
+drawSquare(200,0,100,COLOURS["PURPLE"])
+drawRectangle(100,500,0,400)
 updateLocalBuffer()
 syncBuffers()
