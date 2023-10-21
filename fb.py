da04d4abebdafc4ccbd7717f2ff66938e08f4c8a
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 02:37:04 -0400

    vm

--- fb.py
+++ fb.py
@@ -151,11 +151,10 @@ def drawTicTacToeBoard():
     ]
 
     circle_lookup = [
-            [0,0], [0,1], [0,2],
-            [1,0], [1,1], [1,2],
-            [2,0], [2,1], [2,2]
-    ]
-
+            [0,0], [1,0], [2,0],
+            [0,1], [1,1], [2,1],
+            [0,2], [1,2], [2,2]
+        ]
     def o(index: int) -> None:
         x, y = [round(board_size * line_offset[i]) for i in circle_lookup[index]]
         drawCircle(x,y,20,COLOURS["WHITE"])
