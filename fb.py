0d7156fea35bd415be96b3b939079c932e3efa8d
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 03:25:04 -0400

    vm

--- fb.py
+++ fb.py
@@ -72,12 +72,12 @@ def drawRectangle(start_x: int, start_y: int, end_x: int, end_y: int, colour: by
             queueLocalChange(i, j, colour)
 
 def drawLine(start_x: int, start_y: int, end_x: int, end_y: int, colour: bytes, thickness: int = 3) -> None:
-    m = (end_y-start_y)/(end_x-start_x)
+    slope = (end_y-start_y)/(end_x-start_x)
     c = (start_x*end_x-end_y*start_x)/(end_x-start_x)
     for x in range(end_x-start_x):
         for i in range(thickness):
             # TODO: Fix thickness for x axis
-            y = round(m*(x+start_x+i)+c)
+            y = round(slope*(x+start_x+i)+c)
             queueLocalChange(x,y,colour)
             queueLocalChange(x,y-(i*2),colour) # Nice :)
 
