460df74979170543838b95179bc9f618fdf34500
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 03:56:50 -0400

    vm

--- fb.py
+++ fb.py
@@ -76,7 +76,6 @@ def drawLine(start_x: int, start_y: int, end_x: int, end_y: int, colour: bytes, 
     c = (start_x*end_x-end_y*start_x)/(end_x-start_x)
     length = end_x-start_x
     for x in (range(length) if length > 0 else range(length, 0)):
-        print(x)
         for i in range(thickness):
             # TODO: Fix thickness for x axis
             y = round(slope*(x+start_x+i)+c)
