c9e65bf8c158dcc21cb3a402782a8e9eaa90a4f7
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 16:07:49 -0400

    vm

--- fb.py
+++ fb.py
@@ -87,8 +87,9 @@ def drawLine(start_x: int, start_y: int, end_x: int, end_y: int, colour: bytes, 
         tmp = start_y
         start_y = end_y
         end_y = tmp
-    c = start_y - slope * start_x
+        
     slope = (end_y - start_y) / (end_x - start_x)
+    c = start_y - slope * start_x
     for x in range(start_x, end_x + 1):
         for i in range(thickness+1):
             y = round(slope * (x+i) + c)
