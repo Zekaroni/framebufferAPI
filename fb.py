8b94b2495c4e247121395ad05a5adf36512bd7ae
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 14:34:05 -0400

    vm

--- fb.py
+++ fb.py
@@ -175,7 +175,7 @@ def drawTicTacToeBoard(x_offset: int = 0, y_offset: int = 0) -> None:
             ],
             [
                 x_offset+cross_padding,
-                y_offset+round(board_size*midpoint_offset[0])+cross_padding,
+                y_offset+round(board_size*midpoint_offset[0])-cross_padding,
                 x_offset+round(board_size*midpoint_offset[0])-cross_padding-(line_thickness*2),
                 y_offset+cross_padding+(line_thickness*2),
             ]
