a9b057b907408aaf1d94156e0dec19790e8877b1
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 15:16:29 -0400

    vm

--- fb.py
+++ fb.py
@@ -165,8 +165,8 @@ def drawTicTacToeBoard(x_offset: int = 0, y_offset: int = 0) -> None:
     ]
     section_size = round(board_size/3)
     axis_offsets = [
-        1/12,
-        1/4,
+        .33,
+        .66,
     ]
     cross_lookup = [
         [
@@ -178,7 +178,7 @@ def drawTicTacToeBoard(x_offset: int = 0, y_offset: int = 0) -> None:
             ],
             [
                 x_offset+round(section_size*axis_offsets[0]),
-                y_offset+round(board_size*midpoint_offset[0])+round(section_size*axis_offsets[1]),
+                y_offset+round(board_size*midpoint_offset[0])-round(section_size*axis_offsets[1]),
                 x_offset+round(board_size*midpoint_offset[0])-round(section_size*axis_offsets[0]),
                 y_offset+round(section_size*axis_offsets[1]),
             ]
