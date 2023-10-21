728144369a58b924200337e2044d6ebe2bea89e2
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 16:36:23 -0400

    vm

--- fb.py
+++ fb.py
@@ -95,7 +95,7 @@ def drawLine(start_x: int, start_y: int, end_x: int, end_y: int, colour: bytes, 
     for x in range(start_x, end_x + 1):
         for i in range(thickness+1):
             y = round(slope * (x+i) + (c if not swap else 0))
-            queueLocalChange(x, (y-1 if not swap else y), colour)
+            queueLocalChange(x, (y-i+1 if not swap else y), colour)
             queueLocalChange(x, y - (i * 2), colour)
 
 
