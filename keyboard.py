50fd256bd0b8d80ee287339bdac4fda913da55fd
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-22 15:47:39 -0400

    vm

--- keyboard.py
+++ keyboard.py
@@ -22,6 +22,5 @@ with open(keyboard_device, "rb") as f:
         evtype  = int.from_bytes(bytes(data[17:20]), byteorder='little')
         if evtype and evtype!=UNKNOWN_EVENT:
             code    = int.from_bytes(bytes(data[20:23]), byteorder='little')
-            value   = int.from_bytes(bytes(data[23:]), byteorder='little')  
             print("\x1b[2J\x1b[H",end="")
-            print(f"Type: {KEYS[evtype] if evtype in KEYS else evtype} - Code: {'PRESSED' if code else 'RELEASED'} - Value: {value}       ",end="\r")
+            print(f"Type: {KEYS[evtype] if evtype in KEYS else evtype} - Code: {'PRESSED' if code else 'RELEASED'}       ",end="\r")
