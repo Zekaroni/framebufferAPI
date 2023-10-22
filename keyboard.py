a668b12d490f1acd413d7bdaafc312f7377b24a8
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-21 22:47:59 -0400

    vm

--- keyboard.py
+++ keyboard.py
@@ -1,19 +1,9 @@
-keyboard_device = "/dev/input/event2"  # Replace X with the appropriate device number
-
-# Open the device file in binary mode
+keyboard_device = "/dev/input/event2"
 with open(keyboard_device, "rb") as f:
-    event_size = 24  # Size of each input event
-
+    event_size = 24 
     while True:
         event_data = f.read(event_size)
-
         if len(event_data) != event_size:
             break
-
-        # Extract values from the event data
-        a,b,c = (
-            int.from_bytes(event_data[i:i + 8], byteorder='little')
-            for i in range(0, event_size, 8)
-        )
-
+        a,b,c = (int.from_bytes(event_data[i:i + 8], byteorder='little')for i in range(0, event_size, 8))
         print(a,b,c)
