41e9dd8f2fedf90a4a2949af4ea5510e29eebf3b
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-22 15:30:34 -0400

    vm

--- keyboard.py
+++ keyboard.py
@@ -18,12 +18,10 @@ with open(keyboard_device, "rb") as f:
             break
         data = bytearray(event_data)
         
-        tv_sec  = int.from_bytes(bytes(data[0:9]), byteorder='little')
-        tv_usec = int.from_bytes(bytes(data[9:17]), byteorder='little')
         evtype  = int.from_bytes(bytes(data[17:20]), byteorder='little')
         code    = int.from_bytes(bytes(data[20:23]), byteorder='little')
         value   = int.from_bytes(bytes(data[23:]), byteorder='little')
 
         print(
-            f"Type: {evtype}\nCode: {code}\nValue: {value}"
+            f"Type: {KEYS[evtype] if evtype in KEYS else evtype} - Code: {code} - Value: {value}"
             )
