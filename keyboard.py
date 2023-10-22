79c16d5e4d5f15c991bf8e498c2ddfd4149ce848
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-22 15:09:58 -0400

    vm

--- keyboard.py
+++ keyboard.py
@@ -1,14 +1,22 @@
 keyboard_device = "/dev/input/event2"
+
+# Open the device file in binary mode
 with open(keyboard_device, "rb") as f:
-    event_size = 24 
+    event_size = 24  # Size of each input event (bytes) (I counted 25, maybe misscount)
+
     while True:
         event_data = f.read(event_size)
+
         if len(event_data) != event_size:
             break
-        print(event_data)
-        a,b,c = (int.from_bytes(event_data[i:i + 8], byteorder='little')for i in range(0, event_size, 8))
-        print(
-            f"A = {a}\n",
-            f"B = {b}\n",
-            f"C = {c}",
-            sep="")
+
+        # Parse the event data into its components
+        # Each part corresponds to specific information about the input event
+        time_sec, time_usec, event_type, event_code, event_value = (
+            int.from_bytes(event_data[i:i + 8], byteorder='little')
+            for i in range(0, event_size, 8)
+        )
+
+        if event_type == 1 and event_value == 1:
+            # Key press event
+            print(f"Key pressed: Event Code {event_code}")
