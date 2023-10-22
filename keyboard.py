ee620d6497e45295bedf564701ccee4ae232eee5
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-22 15:26:40 -0400

    vm

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,12 @@
 keyboard_device = "/dev/input/event2"
 
+KEYS = {
+    26880: "LEFT",
+    27648: "DOWN",
+    27136: "RIGHT",
+    26368: "UP",
+}
+
 # Open the device file in binary mode
 with open(keyboard_device, "rb") as f:
     event_size = 24  # Size of each input event (bytes)
@@ -11,11 +18,11 @@ with open(keyboard_device, "rb") as f:
             break
         data = bytearray(event_data)
         
-        tv_sec  = int.from_bytes(bytes(data[0:9]), byteorder='little')
-        tv_usec = int.from_bytes(bytes(data[9:17]), byteorder='little')
-        evtype  = int.from_bytes(bytes(data[17:20]), byteorder='little')
-        code    = int.from_bytes(bytes(data[20:23]), byteorder='little')
-        value   = int.from_bytes(bytes(data[23:]), byteorder='little')
+        tv_sec  = int.from_bytes(bytes(data[0:9]), byteorder='big')
+        tv_usec = int.from_bytes(bytes(data[9:17]), byteorder='big')
+        evtype  = int.from_bytes(bytes(data[17:20]), byteorder='big')
+        code    = int.from_bytes(bytes(data[20:23]), byteorder='big')
+        value   = int.from_bytes(bytes(data[23:]), byteorder='big')
 
         print(
             f"Type: {evtype}\nCode: {code}\nValue: {value}"
