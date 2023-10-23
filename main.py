e34e9e2f616bade17227e550370eea22289e2c2f
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-22 21:36:47 -0400

    vm

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ class BoardLogicHandler:
     
     def resetPreviousTile(self):
         _offset = round(self.boardRenderer.board_size/8)
-        _mid = self.boardRenderer.index_midpoints[self.previous_position]
+        _mid = self.boardRenderer.index_midpoints[self.previousPosition]
         x1, y1, x2, y2 = [
             self.x_offset + _mid[0] - _offset,
             self.y_offset + _mid[1] - _offset,
