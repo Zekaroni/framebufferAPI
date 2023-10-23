d3f4f3ecf0fd34a5bbe6eb5b3287f55ddfbba610
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-23 16:49:06 -0400

    update image conversion

--- extra/convertImage.py
+++ extra/convertImage.py
@@ -3,7 +3,9 @@ from PIL import Image
 def convertRGBtoBGRA(r:int, g:int, b:int) -> bytes:
     r = r << (16)
     g = g << (8)
-    return (r + g + b).to_bytes(4,'little')
+    return (r + g + b).to_bytes(3,'little')
+
+# -25%
 
 
 im = Image.open("image.jpg")
@@ -19,3 +21,4 @@ px = im.load()
 for y in range(height):
     for x in range(width):
         outputFile.write(convertRGBtoBGRA(*px[x,y]))
+outputFile.close()
