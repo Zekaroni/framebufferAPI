fb8efd9e27ddfac0c88a55a41de0155be2ed42c1
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2024-01-01 16:46:33 -0500

    vm test

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -86,9 +86,9 @@ int main()
     {
         for (int o = 0; o < renderEngine.getWidth(); o++)
         {
-            int a = i % 11 * o;
-            int g = i % 7 * o;
-            int r = i % 5 * o;
+            int a = i % 45 * o;
+            int g = i % 27 * o;
+            int r = i % 36 * o;
             uint8_t pixel[4] = {a,g,r,0};
             localbuffer[index]   = pixel[0];
             localbuffer[index+1] = pixel[1];
